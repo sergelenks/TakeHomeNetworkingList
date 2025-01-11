85b71f9b02f2d6c90098392dd183c12762a31801
sergelenks <sergelents93@gmail.com>
2025-01-11 09:15:32 -0800

    Initial Commit

---
+++ FetchTakeHome/FetchTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeApp.swift
+//  FetchTakeHome
+//
+//  Created by Serg Tsogtbaatar on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
